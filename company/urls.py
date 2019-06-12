a52d91eda4d9ea4a8900498a5961125b2765b2a6
Hsekaj B <jakesh.bohaju@gmail.com>
2019-06-12 19:03:23 +0545

    update templates'

--- company/urls.py
+++ company/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from company.views import *
 
 app_names = 'company'
 
 urlpatterns = [
-
+    path('jobpost', JobPostView.as_view(), name='job_post')
 ]
--- job_seeker/views.py
+++ job_seeker/views.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 
 
 class JobListView(TemplateView):
-    template_name = 'job_list.html'
+    template_name = 'seeker_register.html'
 
 
 class CategoryListView(TemplateView):
