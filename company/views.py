8180a2bc700188bb64a4e21a73b248c261724b5a
jakesh-bohaju <jakesh.bohaju@gmail.com>
2019-07-07 17:19:41 +0545

    company dashboard job post, job list, applicant list completed

--- company/views.py
+++ company/views.py
@@ -98,6 +98,8 @@ class JobPostListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        user = self.request.user.id
+        context['joblists'] = JobPost.objects.filter(company__user_id=user)
         return context
 
 
