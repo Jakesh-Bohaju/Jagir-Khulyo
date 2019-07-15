7d135b0e84b7b3f5c209444a2d3ea29a9c634d4b
jakesh-bohaju <jakesh.bohaju@gmail.com>
2019-07-15 14:37:18 +0545

    procfile deleted

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '!b6vyuq%^x^8@2(!p(t2%7ngog-rv_^z+@)7!ru(^_dh8@$hnx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
